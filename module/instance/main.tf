ae846717b8fa7262a6f5fbf7b9ac5d2ea005e2d3
Olugbamila <gbenlebuc@gmail.com>
2025-11-06 23:48:13 -0800

    daniel4

--- module/instance/main.tf
+++ module/instance/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "instance" {
   count         = 1
 
   tags = {
-    Name        = "gbamila - ${count.index + 1}"
+    Name        = "gbamil - ${count.index + 1}"
     Environment = "Dev"
   }
 }
